507f68ebd16fdab1094de8d94e694fcad150997f
nconrad <nealconrad@gmail.com>
2015-06-11 15:17:24 -0500

    use production data api

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ angular.module('config', []).service('config', function() {
                         ms_url: "https://p3.theseed.org/services/ProbModelSEED/",
                         shock_url: "http://p3.theseed.org/services/shock_api",
                         patric_auth_url: "https://user.beta.patricbrc.org/authenticate",
-                        solr_url: "https://www.beta.patricbrc.org/api/"
+                        solr_url: "https://www.patricbrc.org/api/"
                         //fba_url: "https://kbase.us/services/KBaseFBAModeling",
                         //ws_url: "https://kbase.us/services/ws",
                         //auth_url: "https://p3.theseed.org/Sessions/Login",
