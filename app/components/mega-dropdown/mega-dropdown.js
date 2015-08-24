d5b9e7c2a07b695ad7c05fe30e84206ee4365636
nconrad <nealconrad@gmail.com>
2015-08-24 12:25:00 -0500

    refactor sidebar (1 of 2)

---
+++ app/components/mega-dropdown/mega-dropdown.js
@@ -0,0 +1,12 @@
+
+angular.module('mega-dropdown', []);
+angular.module('mega-dropdown')
+.directive('megaDropdown', function() {
+    return {
+        restrict: 'EA',
+        template: '',
+        link: function(scope, elem, attrs) {
+            //console.log('mega-dropdown')
+        }
+    }
+})
