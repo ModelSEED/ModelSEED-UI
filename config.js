9d39371b9e01eb11ae2fbd2f2b0e851e2ec816cc
nconrad <nealconrad@gmail.com>
2015-09-08 19:14:37 -0500

    add ms support endpoint

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ angular.module('config', []).service('config', function() {
                                                      "https://p3.theseed.org/services/ProbModelSEED/",
                         shock_url: "http://p3.theseed.org/services/shock_api",
                         patric_auth_url: "https://user.patricbrc.org/authenticate",
-                        solr_url: "https://www.patricbrc.org/api/"
+                        solr_url: "https://www.patricbrc.org/api/",
+                        ms_support_url: "http://bio-data-1.mcs.anl.gov/services/ms_fba"
                     };
 
 
