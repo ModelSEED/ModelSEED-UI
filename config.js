b8c2a879b087105651d2b507b089f6de9f8be039
nconrad <nealconrad@gmail.com>
2016-06-23 15:06:58 -0500

    changes from plantseed branch with plants off

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ angular.module('config', []).service('config', function() {
 
     // wether or not to include plants UI components
     // see app/ctrls/ctrls.js > "ViewOptions" for localstorage methods
-    this.includePlants = true;
+    this.includePlants = false;
 
     this.services = {
         auth_url: "http://tutorial.theseed.org/Sessions/Login",
         app_url: "http://p3.theseed.org/services/app_service",
         ws_url: "http://p3.theseed.org/services/Workspace",
-        ms_url: "http://p3c.theseed.org/dev1/services/ProbModelSEED", //"https://p3.theseed.org/services/ProbModelSEED/",
+        ms_url: "https://p3.theseed.org/services/ProbModelSEED/",
         ms_rest_url: "http://api.modelseed.org/v0",
         shock_url: "http://p3.theseed.org/services/shock_api",
         patric_auth_url: "https://user.patricbrc.org/authenticate",
